70d53dede6a787a7ca09ecf403b5930328441157
Presh <lukeprecious@gmail.com>
2021-11-08 09:05:58 +0100

    first commit

---
+++ schemas/covid.js
@@ -0,0 +1,23 @@
+export default{
+    name: "covid",
+    type : "document",
+    title: "Covid19Data",
+    fields: [
+      {
+      name: "title",
+      type: "string",
+      title: "States"
+      },
+      {
+        name: "confirmed",
+        type: "number",
+        title: "Confirmed"
+      },
+        {
+          name: "death",
+          type: "number",
+          title: "Death"
+          },
+
+    ],
+  }
